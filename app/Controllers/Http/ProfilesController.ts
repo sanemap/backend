7d47973d39b51fabe2c9b584d7a7111a787dafef
Phablo Raylan <phabloraylan@gmail.com>
2023-10-04 21:45:11 -0300

    autenticação e perfil

---
+++ app/Controllers/Http/ProfilesController.ts
@@ -0,0 +1,11 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class ProfilesController {
+  public async index({
+    auth
+                     }: HttpContextContract) {
+
+    return auth.user;
+  }
+
+}
---
+++ app/Models/User.ts
@@ -0,0 +1,50 @@
+import { DateTime } from 'luxon'
+import Hash from '@ioc:Adonis/Core/Hash'
+import { column, beforeSave, BaseModel, hasMany, HasMany } from '@ioc:Adonis/Lucid/Orm'
+import Publication from 'App/Models/Publication'
+
+export default class User extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public email: string
+
+  @column({ serializeAs: null })
+  public password: string
+
+  @column()
+  public rememberMeToken: string | null
+
+
+  @column()
+  public nome: string
+
+  @column()
+  public cpf: string
+
+  @column()
+  public nascimento: DateTime
+
+  @column()
+  public telefone: string
+
+  @column()
+  public avatar: string
+
+  @hasMany(() => Publication)
+  public publications: HasMany<typeof Publication>
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @beforeSave()
+  public static async hashPassword (user: User) {
+    if (user.$dirty.password) {
+      user.password = await Hash.make(user.password)
+    }
+  }
+}
